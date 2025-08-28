dc8edc1dbc6cfb7aeaae3cf4ec59def4fb2cd57b
pabgarcialopez <pabgarcialopez@gmail.com>
2025-08-28 11:56:15 +0200

    Improved layout of profileView

--- Recipedia/Profile/ProfileViews/ProfileView.swift
+++ Recipedia/Profile/ProfileViews/ProfileView.swift
@@ -7,6 +7,55 @@
 
 import SwiftUI
 
+//struct SettingsSection<Content: View>: View {
+//    let title: String
+//    @ViewBuilder let content: () -> Content
+//    
+//    var body: some View {
+//        VStack(alignment: .leading, spacing: 0) {
+//            Text(title)
+//                .font(.headline)
+//                .padding(.bottom, 8)
+//            
+//            VStack(spacing: 0) {
+//                content()
+//            }
+//            .background(RoundedRectangle(cornerRadius: 12).fill(Color(.systemGray6)))
+//            .overlay(RoundedRectangle(cornerRadius: 12).stroke(Color(.separator)))
+//        }
+//    }
+//}
+
+struct SettingsSection: View {
+    let title: String
+    let items: [AnyView]
+    
+    init(_ title: String, items: [AnyView]) {
+        self.title = title
+        self.items = items
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(title)
+                .font(.headline)
+                .padding(.bottom, 8)
+            
+            VStack(spacing: 0) {
+                ForEach(items.indices, id: \.self) { idx in
+                    items[idx]
+                    if idx < items.count - 1 { Divider() }
+                }
+            }
+            .background(RoundedRectangle(cornerRadius: 12).fill(Color(.systemGray6)))
+            .overlay(RoundedRectangle(cornerRadius: 12).stroke(Color(.separator)))
+        }
+        .padding(.vertical, 6)
+    }
+}
+
+
+
 struct ProfileView: View {
     
     @ObservedObject var profileViewModel: ProfileViewModel
@@ -23,7 +72,7 @@ struct ProfileView: View {
     
     private var profileCard: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 22)
+            RoundedRectangle(cornerRadius: 15)
                 .fill(.blue.opacity(0.15).shadow(.drop(color: .black, radius: 5, x: 5, y: 5)))
                 .stroke(.blue, lineWidth: 2)
             
@@ -56,6 +105,7 @@ struct ProfileView: View {
             .padding()
         }
     }
+    
     private var signOutButton: some View {
         return Button(action: profileViewModel.signOut) {
             HStack {
@@ -63,7 +113,6 @@ struct ProfileView: View {
                 Text("Sign out")
             }
             .frame(maxWidth: .infinity)
-            .bold()
             .foregroundStyle(.black)
             .padding()
             .background(.red.opacity(0.2))
@@ -78,10 +127,26 @@ struct ProfileView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack {
+                VStack(alignment: .leading) {
+                    
                     profileCard
                     
-                    profileNavigationLink("Account", icon: "person.fill", destination: EmptyView())
+                    SettingsSection("Your account", items: [
+                            AnyView(profileNavigationLink("Account", icon: "person.fill", destination: EmptyView())),
+                        ]
+                    )
+                    
+                    SettingsSection("App related", items: [
+                            AnyView(profileNavigationLink("Favorites", icon: "heart.fill", destination: EmptyView())),
+                        ]
+                    )
+                    
+                    SettingsSection("Support & Info", items: [
+                            AnyView(profileNavigationLink("Terms and conditions", icon: "list.bullet", destination: EmptyView())),
+                            AnyView(profileNavigationLink("About the app", icon: "info.circle", destination: EmptyView()))
+                        ]
+                    )
+
                     
                     signOutButton
                 }
@@ -96,22 +161,22 @@ struct ProfileView: View {
         return NavigationLink {
             destination
         } label: {
-            ZStack {
-                RoundedRectangle(cornerRadius: 22)
-                    .fill(.clear.shadow(.drop(color: .black, radius: 5, x: 5, y: 5)))
-                    .stroke(.black, lineWidth: 2)
-                
-                HStack {
-                    Image(systemName: icon)
-                    Text(title)
-                    Spacer()
-                    Image(systemName: "chevron.forward")
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundStyle(.black)
-                .padding()
+            HStack {
+                Image(systemName: icon)
+                    .padding(8)
+                    .background(Color(UIColor.systemGray4))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .frame(width: 30)
+                Text(title)
+                    .padding(.leading, 5)
+                Spacer()
+                Image(systemName: "chevron.forward")
+                    .padding(.trailing, 4)
             }
-            
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundStyle(.black)
+            .padding(.vertical, 12)
+            .padding(.horizontal, 15)
         }
     }
 
